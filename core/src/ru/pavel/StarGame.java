ff9cb28f0ed5b31a0eb2ce4d60df3d166c265604
Pavel <pavelmth@gmail.com>
2019-01-18 09:55:12 +0300

    L02_1

--- core/src/ru/pavel/StarGame.java
+++ core/src/ru/pavel/StarGame.java
@@ -1,17 +1,22 @@
 package ru.pavel;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StarGame extends ApplicationAdapter {
+import ru.pavel.screen.MenuScreen;
+
+//public class StarGame extends ApplicationAdapter {
+public class StarGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		setScreen(new MenuScreen());
 		batch = new SpriteBatch();
 		img = new Texture("zvezdnoe_nebo.jpg");
 	}
---
+++ core/src/ru/pavel/screen/MenuScreen.java
@@ -0,0 +1,6 @@
+package ru.pavel.screen;
+
+import ru.pavel.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+}
